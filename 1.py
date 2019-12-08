a3e3c5bf35eab9b713e33786bb9d82d1426e9caa
pitikdmitry <pitik.dmitry@mail.ru>
2019-12-08 20:03:37 +0300

    xren2

--- 1.py
+++ 1.py
@@ -3,8 +3,13 @@ import logging
 
 
 logging.error("FGEWHIBNGWEIBG")
+logging.error("FGEWHIBNGWEIBG")
+logging.error("FGEWHIBNGWEIBG")
 
 with open('/app/logs/1.txt', 'w') as f:
-    f.write('fgerwgerwgeerg')
+    f.write('r345')
+    f.write('34543')
+    f.write('534')
+    f.write('5234')
 
 sleep(100)
